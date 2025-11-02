a0096ad0a646b4583cb6a038dee36fefced32187
Minjae Sung <137097875+OptimisticPotato@users.noreply.github.com>
2025-11-02 13:23:36 +0900

    Add files via upload
    week 10 source code

---
+++ Week_10/SAMPLING_TIME_2.c
@@ -0,0 +1,27 @@
+/* SAMPLING_TIME_2 */
+
+#include <stdio.h>
+#include <wiringPi.h>
+
+int main()
+{
+    wiringPiSetupGpio();
+    unsigned int startTime = millis();
+
+    unsigned int checkTimeBefore = millis();
+    unsigned int checkTime;
+
+    int cnt = 0;
+
+    while(cnt < 100)
+    {
+        checkTime = millis();
+        if (checkTime - checkTimeBefore >= 5) // 5 msec
+        {
+            cnt++;
+            printf("loop time: %d msec, After init: %d msec\n", checkTime - checkTimeBefore, checkTime - startTime);
+            checkTimeBefore = checkTime;
+        }
+    }
+    return 0;
+}
